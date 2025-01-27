ed7286f237aefa5f1e144b9b379cb92d91f51c25
Jay Bosamiya <jayb@microsoft.com>
2025-01-27 14:29:06 -0800

    Support convenient global platforms for shims

---
+++ litebox_platform_multiplex/src/lib.rs
@@ -0,0 +1,58 @@
+//! A multiplexer for [LiteBox platforms](../litebox/platform/index.html), to simplify access to a
+//! global platform for shims "above" LiteBox.
+//!
+//! At a high level, due to Rust language design decisions, supporting a **global**
+//! runtime-parametric platform is not quite directly feasible. In particular, either
+//! platform-dependent functionality either needs to be aware of the platform, or each function
+//! would need a parametric platform, neither of which is ideal. This crate side-steps that by using
+//! conditional compilation (at this single crate) to provide the necessary switching between
+//! platforms.
+//!
+//! Specifically, a platform MUST be selected via one of the features provided by this crate (and
+//! cannot be provided dynamically at run-time). However, crates above it can then work with a
+//! global platform _without_ needing to deal with any such switching. If a LiteBox platform exists
+//! that does not have a corresponding feature in this crate, support for it is easy to add.
+//!
+//! By default, this crate picks the Linux userland platform.
+
+#![no_std]
+
+extern crate alloc;
+
+// Checking if more than one of the platforms has been specified. If so, compiler error.
+//
+// NOTE: Currently, we only support one platform, thus this is a trivial no-op. However, once we
+// have more, we must account for each of the possible pairs.
+#[cfg(any())]
+compile_error!(
+    r##"Too many platforms specified. Are you sure you have marked 'default-features = false'?"##
+);
+
+// Check if no platforms have been specified. If so, compiler error.
+#[cfg(not(any(feature = "platform_linux_userland")))]
+compile_error!(
+    r##"No platforms specified.  Please enable the feature for the platform you want."##
+);
+
+#[cfg(feature = "platform_linux_userland")]
+pub type Platform = litebox_platform_linux_userland::LinuxUserland;
+
+static PLATFORM: once_cell::race::OnceBox<Platform> = once_cell::race::OnceBox::new();
+
+/// Initialize the shim by providing a [LiteBox platform](../litebox/platform/index.html).
+///
+/// **Must** be invoked prior to any of the other functionality provided by this crate; all other
+/// functionality is prone to panics if this has not been invoked first.
+pub fn set_platform(platform: Platform) {
+    match PLATFORM.set(alloc::boxed::Box::new(platform)) {
+        Ok(()) => {}
+        Err(_) => panic!("set_platform should only be called once per crate"),
+    }
+}
+
+/// Get the global platform, or panic if [`set_platform`] has not yet been invoked.
+pub fn platform<'a>() -> &'a Platform {
+    PLATFORM
+        .get()
+        .expect("set_platform should have already been called before this point")
+}
