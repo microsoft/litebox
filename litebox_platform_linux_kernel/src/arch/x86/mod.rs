c1a968d7842ab880fbfa493ea6410c6663940dc0
weitengchen <weitengchen@microsoft.com>
2025-03-05 18:48:55 -0800

    fix import

--- litebox_platform_linux_kernel/src/arch/x86/mod.rs
+++ litebox_platform_linux_kernel/src/arch/x86/mod.rs
@@ -1,12 +1,12 @@
 pub mod mm;
 
-pub use x86_64::{
+pub(crate) use x86_64::{
     addr::{PhysAddr, VirtAddr},
-    structures::{
-        idt::PageFaultErrorCode,
-        paging::{
-            Page, PageSize, PageTableFlags, PhysFrame, Size4KiB, mapper::TranslateResult,
-            page::PageRange,
-        },
-    },
+    structures::paging::{PageSize, Size4KiB},
+};
+
+#[cfg(test)]
+pub(crate) use x86_64::structures::{
+    idt::PageFaultErrorCode,
+    paging::{Page, PageTableFlags, PhysFrame, mapper::TranslateResult},
 };
