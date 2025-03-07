92c5cb8d1c8cc4b39eed759adae031519f4726be
Jay Bosamiya <jayb@microsoft.com>
2025-03-07 15:53:13 -0800

    Tests to confirm file position maintenance during read/write ops

--- litebox/src/fs/tests.rs
+++ litebox/src/fs/tests.rs
@@ -309,10 +309,60 @@ mod layered {
         fs.write(&fd2, b"share", None)
             .expect("Failed to write to file");
 
+        fs.seek(&fd1, 0, crate::fs::SeekWhence::RelativeToBeginning)
+            .expect("Failed to seek to start");
         let bytes_read = fs
             .read(&fd1, &mut buffer, None)
             .expect("Failed to read from file");
         assert_eq!(&buffer[..bytes_read], b"shareoo\n");
+
+        fs.close(fd1).expect("Failed to close file");
+        fs.close(fd2).expect("Failed to close file");
+    }
+
+    /// Similar to [`file_read_write_sync_up`] but also confirm that file positions have been
+    /// maintained.
+    #[test]
+    fn file_read_write_seek_sync() {
+        let platform = MockPlatform::new();
+
+        let mut in_mem_fs = in_mem::FileSystem::new(&platform);
+        in_mem_fs.with_root_privileges(|fs| {
+            // Change the permissions for `/` to allow file creation
+            //
+            // TODO: We might need to force-allow file creation in cases where the lower level
+            // already has the file in the correct mode. This would likely require `stat` as well as
+            // some internal-only force-creation API.
+            fs.chmod("/", Mode::RWXU | Mode::RWXG | Mode::RWXO)
+                .expect("Failed to chmod /");
+        });
+
+        let mut fs = layered::FileSystem::new(
+            &platform,
+            in_mem_fs,
+            tar_ro::FileSystem::new(&platform, TEST_TAR_FILE.into()),
+        );
+        let fd1 = fs
+            .open("foo", OFlags::RDONLY, Mode::RWXU)
+            .expect("Failed to open file");
+        let fd2 = fs
+            .open("foo", OFlags::WRONLY, Mode::RWXU)
+            .expect("Failed to open file");
+
+        let mut buffer = vec![0; 4];
+
+        let bytes_read = fs
+            .read(&fd1, &mut buffer, None)
+            .expect("Failed to read from file");
+        assert_eq!(&buffer[..bytes_read], b"test");
+
+        fs.write(&fd2, b"share", None)
+            .expect("Failed to write to file");
+
+        let bytes_read = fs
+            .read(&fd1, &mut buffer, None)
+            .expect("Failed to read from file");
+        assert_eq!(&buffer[..bytes_read], b"eoo\n");
 
         fs.close(fd1).expect("Failed to close file");
         fs.close(fd2).expect("Failed to close file");
