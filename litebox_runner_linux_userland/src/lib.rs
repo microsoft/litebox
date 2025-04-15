1a7b2b44ccb7eb0b9ba888cdb6a674bfd1c2eaa0
Jay Bosamiya <jayb@microsoft.com>
2025-04-15 15:55:49 -0700

    Switch to a library with trivial `main` to support future tests

---
+++ litebox_runner_linux_userland/src/lib.rs
@@ -0,0 +1,163 @@
+use anyhow::{Result, anyhow};
+use clap::Parser;
+use litebox::fs::FileSystem as _;
+use litebox_platform_multiplex::Platform;
+use std::os::linux::fs::MetadataExt as _;
+use std::path::PathBuf;
+
+/// Run Linux programs with LiteBox on unmodified Linux
+#[derive(Parser, Debug)]
+pub struct CliArgs {
+    /// The program and arguments passed to it (e.g., `python3 --version`)
+    #[arg(required = true, trailing_var_arg = true, value_hint = clap::ValueHint::CommandWithArguments)]
+    pub program_and_arguments: Vec<String>,
+    /// Environment variables passed to the program (`K=V` pairs; can be invoked multiple times)
+    #[arg(long = "env")]
+    pub environment_variables: Vec<String>,
+    /// Forward the existing environment variables
+    #[arg(long = "forward-env")]
+    pub forward_environment_variables: bool,
+    /// Allow using unstable options
+    #[arg(short = 'Z', long = "unstable")]
+    pub unstable: bool,
+    /// Pre-fill files into the initial file system state
+    // TODO: Might want to extend this to support full directories at some point?
+    #[arg(long = "insert-file", value_hint = clap::ValueHint::FilePath,
+          requires = "unstable", help_heading = "Unstable Options")]
+    pub insert_files: Vec<PathBuf>,
+    /// Pre-fill the files in this tar file into the initial file system state
+    #[arg(long = "initial-files", value_name = "PATH_TO_TAR", value_hint = clap::ValueHint::FilePath,
+          requires = "unstable", help_heading = "Unstable Options")]
+    pub initial_files: Option<PathBuf>,
+    /// Apply syscall-rewriter to the ELF file before running it
+    ///
+    /// This is meant as a convenience feature; real deployments would likely prefer ahead-of-time
+    /// rewrite things to amortize costs.
+    #[arg(
+        long = "rewrite-syscalls",
+        requires = "unstable",
+        help_heading = "Unstable Options"
+    )]
+    pub rewrite_syscalls: bool,
+}
+
+/// Run Linux programs with LiteBox on unmodified Linux
+///
+/// # Panics
+///
+/// Can panic if any particulars of the environment are not set up as expected. Ideally, would not
+/// panic. If it does actually panic, then ping the authors of LiteBox, and likely a better error
+/// message could be thrown instead.
+pub fn run(cli_args: CliArgs) -> Result<()> {
+    if !cli_args.insert_files.is_empty() {
+        unimplemented!(
+            "this should (hopefully soon) have a nicer interface to support loading in files"
+        )
+    }
+
+    // TODO(jb): Clean up platform initialization once we have https://github.com/MSRSSP/litebox/issues/24
+    let platform = Box::leak(Box::new(Platform::new(
+        None,
+        litebox::platform::trivial_providers::ImpossiblePunchthroughProvider {},
+        litebox_shim_linux::syscall_entry,
+    )));
+    let initial_file_system = {
+        let mut in_mem = litebox::fs::in_mem::FileSystem::new(&*platform);
+        in_mem.with_root_privileges(|fs| {
+            let prog = PathBuf::from(&cli_args.program_and_arguments[0]);
+            let ancestors: Vec<_> = prog.ancestors().collect();
+            for path in ancestors.into_iter().skip(1).rev() {
+                fs.mkdir(
+                    path.to_str().unwrap(),
+                    litebox::fs::Mode::from_bits(path.metadata().unwrap().st_mode()).unwrap(),
+                )
+                .unwrap();
+            }
+            let fd = fs
+                .open(
+                    prog.to_str().unwrap(),
+                    litebox::fs::OFlags::WRONLY | litebox::fs::OFlags::CREAT,
+                    litebox::fs::Mode::from_bits(prog.metadata().unwrap().st_mode()).unwrap(),
+                )
+                .unwrap();
+            let prog_data = std::fs::read(prog).unwrap();
+            let data = if cli_args.rewrite_syscalls {
+                litebox_syscall_rewriter::hook_syscalls_in_elf(&prog_data, None).unwrap()
+            } else {
+                prog_data
+            };
+            let mut data = data.as_slice();
+            while !data.is_empty() {
+                let len = fs.write(&fd, data, None).unwrap();
+                data = &data[len..];
+            }
+            fs.close(fd).unwrap();
+        });
+        let tar_data = if let Some(tar_file) = cli_args.initial_files.as_ref() {
+            if tar_file.extension().and_then(|x| x.to_str()) != Some("tar") {
+                anyhow::bail!("Expected a .tar file, found {}", tar_file.display());
+            }
+            std::fs::read(tar_file)
+                .map_err(|e| anyhow!("Could not read tar file at {}: {}", tar_file.display(), e))?
+        } else {
+            litebox::fs::tar_ro::empty_tar_file()
+        };
+        let tar_ro = litebox::fs::tar_ro::FileSystem::new(&*platform, tar_data);
+        litebox::fs::layered::FileSystem::new(&*platform, in_mem, tar_ro)
+    };
+    litebox_shim_linux::set_fs(initial_file_system);
+    litebox_platform_multiplex::set_platform(platform);
+
+    let argv = cli_args
+        .program_and_arguments
+        .iter()
+        .map(|x| std::ffi::CString::new(x.bytes().collect::<Vec<u8>>()).unwrap())
+        .collect();
+    let envp: Vec<_> = cli_args
+        .environment_variables
+        .iter()
+        .map(|x| std::ffi::CString::new(x.bytes().collect::<Vec<u8>>()).unwrap())
+        .collect();
+    let envp = if cli_args.forward_environment_variables {
+        envp.into_iter()
+            .chain(std::env::vars().map(|(k, v)| {
+                std::ffi::CString::new(
+                    k.bytes()
+                        .chain([b'='])
+                        .chain(v.bytes())
+                        .collect::<Vec<u8>>(),
+                )
+                .unwrap()
+            }))
+            .collect()
+    } else {
+        envp
+    };
+
+    let loaded_program =
+        litebox_shim_linux::loader::load_program(&cli_args.program_and_arguments[0], argv, envp)
+            .unwrap();
+
+    unsafe {
+        trampoline::jump_to_entry_point(loaded_program.entry_point, loaded_program.user_stack_top)
+    }
+}
+
+mod trampoline {
+    core::arch::global_asm!(
+        "
+    .text
+    .align  4
+    .globl  jump_to_entry_point
+    .type   jump_to_entry_point,@function
+jump_to_entry_point:
+    xor rdx, rdx
+    mov     rsp, rsi
+    jmp     rdi
+    /* Should not reach. */
+    hlt"
+    );
+    unsafe extern "C" {
+        pub(crate) fn jump_to_entry_point(entry_point: usize, stack_pointer: usize) -> !;
+    }
+}
