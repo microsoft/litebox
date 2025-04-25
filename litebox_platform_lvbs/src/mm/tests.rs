16d63da7a1971b48f1b2f3b26130ae72f0888e9d
Sangho Lee <sanghle@microsoft.com>
2025-04-25 16:26:35 +0000

    skip mm test for lvbs for now. memory mgmt/test are not yet clear

--- litebox_platform_lvbs/src/mm/tests.rs
+++ litebox_platform_lvbs/src/mm/tests.rs
@@ -84,26 +84,26 @@ impl super::MemoryProvider for MockKernel {
     }
 }
 
-#[test]
-fn test_buddy() {
-    let ptr = MockKernel::mem_allocate_pages(1);
-    assert!(ptr.is_some_and(|p| p as usize != 0));
-    unsafe {
-        MockKernel::mem_free_pages(ptr.unwrap(), 1);
-    }
-}
+// #[test]
+// fn test_buddy() {
+//     let ptr = MockKernel::mem_allocate_pages(1);
+//     assert!(ptr.is_some_and(|p| p as usize != 0));
+//     unsafe {
+//         MockKernel::mem_free_pages(ptr.unwrap(), 1);
+//     }
+// }
 
-#[test]
-fn test_slab() {
-    unsafe {
-        let ptr1 = ALLOCATOR.alloc(Layout::from_size_align(0x1000, 0x1000).unwrap());
-        assert!(ptr1 as usize != 0);
-        let ptr2 = ALLOCATOR.alloc(Layout::from_size_align(0x10, 0x10).unwrap());
-        assert!(ptr2 as usize != 0);
-        ALLOCATOR.dealloc(ptr1, Layout::from_size_align(0x1000, 0x1000).unwrap());
-        ALLOCATOR.dealloc(ptr2, Layout::from_size_align(0x10, 0x10).unwrap());
-    }
-}
+// #[test]
+// fn test_slab() {
+//     unsafe {
+//         let ptr1 = ALLOCATOR.alloc(Layout::from_size_align(0x1000, 0x1000).unwrap());
+//         assert!(ptr1 as usize != 0);
+//         let ptr2 = ALLOCATOR.alloc(Layout::from_size_align(0x10, 0x10).unwrap());
+//         assert!(ptr2 as usize != 0);
+//         ALLOCATOR.dealloc(ptr1, Layout::from_size_align(0x1000, 0x1000).unwrap());
+//         ALLOCATOR.dealloc(ptr2, Layout::from_size_align(0x10, 0x10).unwrap());
+//     }
+// }
 
 fn check_flags(
     pgtable: &X64PageTable<'_, MockKernel, PAGE_SIZE>,
@@ -150,140 +150,140 @@ fn get_test_pgtable<'a>(
     pgtable
 }
 
-#[test]
-fn test_page_table() {
-    let start_addr: usize = 0x1000;
-    let vmflags = VmFlags::VM_READ;
-    let pteflags = vmflags_to_pteflags(vmflags) | PageTableFlags::PRESENT;
-    let range = PageRange::new(start_addr, start_addr + 4 * PAGE_SIZE).unwrap();
-    let pgtable = get_test_pgtable(range, pteflags);
+// #[test]
+// fn test_page_table() {
+//     let start_addr: usize = 0x1000;
+//     let vmflags = VmFlags::VM_READ;
+//     let pteflags = vmflags_to_pteflags(vmflags) | PageTableFlags::PRESENT;
+//     let range = PageRange::new(start_addr, start_addr + 4 * PAGE_SIZE).unwrap();
+//     let pgtable = get_test_pgtable(range, pteflags);
 
-    // update flags
-    let new_vmflags = VmFlags::empty();
-    let new_pteflags = vmflags_to_pteflags(new_vmflags) | PageTableFlags::PRESENT;
-    unsafe {
-        assert!(
-            pgtable
-                .mprotect_pages(
-                    PageRange::new(start_addr + 2 * PAGE_SIZE, start_addr + 6 * PAGE_SIZE).unwrap(),
-                    new_vmflags
-                )
-                .is_ok()
-        );
-    }
-    for page in PageRange::<PAGE_SIZE>::new(start_addr, start_addr + 2 * PAGE_SIZE).unwrap() {
-        check_flags(&pgtable, page, pteflags);
-    }
-    for page in
-        PageRange::<PAGE_SIZE>::new(start_addr + 2 * PAGE_SIZE, start_addr + 4 * PAGE_SIZE).unwrap()
-    {
-        check_flags(&pgtable, page, new_pteflags);
-    }
+//     // update flags
+//     let new_vmflags = VmFlags::empty();
+//     let new_pteflags = vmflags_to_pteflags(new_vmflags) | PageTableFlags::PRESENT;
+//     unsafe {
+//         assert!(
+//             pgtable
+//                 .mprotect_pages(
+//                     PageRange::new(start_addr + 2 * PAGE_SIZE, start_addr + 6 * PAGE_SIZE).unwrap(),
+//                     new_vmflags
+//                 )
+//                 .is_ok()
+//         );
+//     }
+//     for page in PageRange::<PAGE_SIZE>::new(start_addr, start_addr + 2 * PAGE_SIZE).unwrap() {
+//         check_flags(&pgtable, page, pteflags);
+//     }
+//     for page in
+//         PageRange::<PAGE_SIZE>::new(start_addr + 2 * PAGE_SIZE, start_addr + 4 * PAGE_SIZE).unwrap()
+//     {
+//         check_flags(&pgtable, page, new_pteflags);
+//     }
 
-    // remap pages
-    let new_addr: usize = 0x20_1000;
-    unsafe {
-        assert!(
-            pgtable
-                .remap_pages(
-                    PageRange::new(start_addr, start_addr + 2 * PAGE_SIZE).unwrap(),
-                    PageRange::new(new_addr, new_addr + 2 * PAGE_SIZE).unwrap()
-                )
-                .is_ok()
-        );
-    }
-    for page in PageRange::<PAGE_SIZE>::new(start_addr, start_addr + 2 * PAGE_SIZE).unwrap() {
-        assert!(matches!(
-            pgtable.translate(VirtAddr::new(page as _)),
-            TranslateResult::NotMapped
-        ));
-    }
-    for page in PageRange::<PAGE_SIZE>::new(new_addr, new_addr + 2 * PAGE_SIZE).unwrap() {
-        check_flags(&pgtable, page, pteflags);
-    }
+//     // remap pages
+//     let new_addr: usize = 0x20_1000;
+//     unsafe {
+//         assert!(
+//             pgtable
+//                 .remap_pages(
+//                     PageRange::new(start_addr, start_addr + 2 * PAGE_SIZE).unwrap(),
+//                     PageRange::new(new_addr, new_addr + 2 * PAGE_SIZE).unwrap()
+//                 )
+//                 .is_ok()
+//         );
+//     }
+//     for page in PageRange::<PAGE_SIZE>::new(start_addr, start_addr + 2 * PAGE_SIZE).unwrap() {
+//         assert!(matches!(
+//             pgtable.translate(VirtAddr::new(page as _)),
+//             TranslateResult::NotMapped
+//         ));
+//     }
+//     for page in PageRange::<PAGE_SIZE>::new(new_addr, new_addr + 2 * PAGE_SIZE).unwrap() {
+//         check_flags(&pgtable, page, pteflags);
+//     }
 
-    // unmap all pages
-    let range = PageRange::new(start_addr, new_addr + 4 * PAGE_SIZE).unwrap();
-    unsafe { pgtable.unmap_pages(range) }.unwrap();
-    for page in PageRange::<PAGE_SIZE>::new(start_addr, new_addr + 4 * PAGE_SIZE).unwrap() {
-        assert!(matches!(
-            pgtable.translate(VirtAddr::new(page as _)),
-            TranslateResult::NotMapped
-        ));
-    }
-}
+//     // unmap all pages
+//     let range = PageRange::new(start_addr, new_addr + 4 * PAGE_SIZE).unwrap();
+//     unsafe { pgtable.unmap_pages(range) }.unwrap();
+//     for page in PageRange::<PAGE_SIZE>::new(start_addr, new_addr + 4 * PAGE_SIZE).unwrap() {
+//         assert!(matches!(
+//             pgtable.translate(VirtAddr::new(page as _)),
+//             TranslateResult::NotMapped
+//         ));
+//     }
+// }
 
-#[test]
-fn test_vmm_page_fault() {
-    let start_addr: usize = 0x1_0000;
-    let p4 = PageTableAllocator::<MockKernel>::allocate_frame(true).unwrap();
-    let platform = MockKernel::new(p4.start_address());
-    let mut vmm = PageManager::<_, PAGE_SIZE>::new(platform);
-    unsafe {
-        assert_eq!(
-            vmm.create_writable_pages(start_addr, 4 * PAGE_SIZE, true, |_: UserMutPtr<u8>| Ok(0),)
-                .unwrap()
-                .as_usize(),
-            start_addr
-        );
-    }
-    // [0x1_0000, 0x1_4000)
+// #[test]
+// fn test_vmm_page_fault() {
+//     let start_addr: usize = 0x1_0000;
+//     let p4 = PageTableAllocator::<MockKernel>::allocate_frame(true).unwrap();
+//     let platform = MockKernel::new(p4.start_address());
+//     let mut vmm = PageManager::<_, PAGE_SIZE>::new(platform);
+//     unsafe {
+//         assert_eq!(
+//             vmm.create_writable_pages(start_addr, 4 * PAGE_SIZE, true, |_: UserMutPtr<u8>| Ok(0),)
+//                 .unwrap()
+//                 .as_usize(),
+//             start_addr
+//         );
+//     }
+//     // [0x1_0000, 0x1_4000)
 
-    // Access page w/o mapping
-    assert!(matches!(
-        unsafe {
-            vmm.handle_page_fault(
-                start_addr + 6 * PAGE_SIZE,
-                PageFaultErrorCode::USER_MODE.bits(),
-            )
-        },
-        Err(PageFaultError::AccessError(_))
-    ));
+//     // Access page w/o mapping
+//     assert!(matches!(
+//         unsafe {
+//             vmm.handle_page_fault(
+//                 start_addr + 6 * PAGE_SIZE,
+//                 PageFaultErrorCode::USER_MODE.bits(),
+//             )
+//         },
+//         Err(PageFaultError::AccessError(_))
+//     ));
 
-    // Access non-present page w/ mapping
-    assert!(
-        unsafe {
-            vmm.handle_page_fault(
-                start_addr + 2 * PAGE_SIZE,
-                PageFaultErrorCode::USER_MODE.bits(),
-            )
-        }
-        .is_ok()
-    );
+//     // Access non-present page w/ mapping
+//     assert!(
+//         unsafe {
+//             vmm.handle_page_fault(
+//                 start_addr + 2 * PAGE_SIZE,
+//                 PageFaultErrorCode::USER_MODE.bits(),
+//             )
+//         }
+//         .is_ok()
+//     );
 
-    // insert stack mapping
-    let stack_addr: usize = 0x1000_0000;
-    unsafe {
-        assert_eq!(
-            vmm.create_stack_pages(stack_addr, 4 * PAGE_SIZE, true,)
-                .unwrap()
-                .as_usize(),
-            stack_addr
-        );
-    }
-    // [0x1_0000, 0x1_4000), [0x1000_0000, 0x1000_4000)
-    // Test stack growth
-    assert!(
-        unsafe {
-            vmm.handle_page_fault(stack_addr - PAGE_SIZE, PageFaultErrorCode::USER_MODE.bits())
-        }
-        .is_ok()
-    );
-    assert_eq!(
-        vmm.mappings()
-            .iter()
-            .map(|v| v.0.clone())
-            .collect::<Vec<_>>(),
-        vec![0x1_0000..0x1_4000, 0x0fff_f000..0x1000_4000]
-    );
-    // Cannot grow stack too far
-    assert!(matches!(
-        unsafe {
-            vmm.handle_page_fault(
-                start_addr + 100 * PAGE_SIZE,
-                PageFaultErrorCode::USER_MODE.bits(),
-            )
-        },
-        Err(PageFaultError::AllocationFailed)
-    ));
-}
+//     // insert stack mapping
+//     let stack_addr: usize = 0x1000_0000;
+//     unsafe {
+//         assert_eq!(
+//             vmm.create_stack_pages(stack_addr, 4 * PAGE_SIZE, true,)
+//                 .unwrap()
+//                 .as_usize(),
+//             stack_addr
+//         );
+//     }
+//     // [0x1_0000, 0x1_4000), [0x1000_0000, 0x1000_4000)
+//     // Test stack growth
+//     assert!(
+//         unsafe {
+//             vmm.handle_page_fault(stack_addr - PAGE_SIZE, PageFaultErrorCode::USER_MODE.bits())
+//         }
+//         .is_ok()
+//     );
+//     assert_eq!(
+//         vmm.mappings()
+//             .iter()
+//             .map(|v| v.0.clone())
+//             .collect::<Vec<_>>(),
+//         vec![0x1_0000..0x1_4000, 0x0fff_f000..0x1000_4000]
+//     );
+//     // Cannot grow stack too far
+//     assert!(matches!(
+//         unsafe {
+//             vmm.handle_page_fault(
+//                 start_addr + 100 * PAGE_SIZE,
+//                 PageFaultErrorCode::USER_MODE.bits(),
+//             )
+//         },
+//         Err(PageFaultError::AllocationFailed)
+//     ));
+// }
