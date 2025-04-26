d3ef86f930a0b4c806dffe9589a20ee0cee72334
Sangho Lee <sanghle@microsoft.com>
2025-04-26 21:55:29 +0000

    no spin lock for bootparams page

--- litebox_runner_lvbs/src/lib.rs
+++ litebox_runner_lvbs/src/lib.rs
@@ -1,6 +1,5 @@
 #![no_std]
 
-use lazy_static::lazy_static;
 use linux_boot_params::{BootE820Entry, BootParams, E820Type};
 use litebox_platform_lvbs::{
     arch::{gdt, interrupts},
@@ -9,7 +8,6 @@ use litebox_platform_lvbs::{
         vtl1_mem_layout::{VTL1_BOOT_PARAMS_PAGE, get_address_of_special_page},
     },
 };
-use spin::Mutex;
 
 pub fn per_core_init() {
     gdt::init();
@@ -18,7 +16,7 @@ pub fn per_core_init() {
 }
 
 struct BootParamsWrapper {
-    page: &'static mut BootParams,
+    page: &'static BootParams,
 }
 
 impl BootParamsWrapper {
@@ -27,7 +25,7 @@ impl BootParamsWrapper {
         self.page.e820_table
     }
 
-    fn ram_addr_size(&mut self) -> (u64, u64) {
+    fn ram_addr_size(&self) -> (u64, u64) {
         for entry in self.page.e820_table {
             let typ = entry.typ;
             if typ == E820Type::Ram {
@@ -39,14 +37,13 @@ impl BootParamsWrapper {
     }
 }
 
-lazy_static! {
-    static ref BOOT_PARAMS_PAGE: Mutex<BootParamsWrapper> = Mutex::new(BootParamsWrapper {
+// the BootParams page is free from data race because it is read only at VTL1 boot
+// and ignored later.
+pub fn get_vtl1_base_address_size() -> (u64, u64) {
+    let boot_params_wrapper = BootParamsWrapper {
         page: unsafe {
-            &mut *(get_address_of_special_page(VTL1_BOOT_PARAMS_PAGE) as *mut BootParams)
-        }
-    });
-}
-
-pub fn ram_addr_size() -> (u64, u64) {
-    BOOT_PARAMS_PAGE.lock().ram_addr_size()
+            &*(get_address_of_special_page(VTL1_BOOT_PARAMS_PAGE) as *const BootParams)
+        },
+    };
+    boot_params_wrapper.ram_addr_size()
 }
