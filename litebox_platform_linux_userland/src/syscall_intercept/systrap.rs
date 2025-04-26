ac7692c3bd14af28fd91d75d95c09e0eea8e8bb4
weitengchen <weitengchen@microsoft.com>
2025-04-25 21:56:39 -0700

    fix socket type and flags

--- litebox_platform_linux_userland/src/syscall_intercept/systrap.rs
+++ litebox_platform_linux_userland/src/syscall_intercept/systrap.rs
@@ -299,18 +299,16 @@ unsafe extern "C" fn syscall_dispatcher(syscall_number: i64, args: *const usize)
         },
         libc::SYS_socket => {
             let domain: u32 = syscall_args[0].truncate();
-            let ty: u32 = syscall_args[1].truncate();
+            let type_and_flags: u32 = syscall_args[1].truncate();
             SyscallRequest::Socket {
                 domain: litebox_common_linux::AddressFamily::from(domain),
-                ty: litebox_common_linux::SockType::from(ty),
-                flags: litebox_common_linux::SockFlags::from_bits_truncate(
-                    syscall_args[2].truncate(),
-                ),
-                protocol: if syscall_args[3] == 0 {
+                ty: litebox_common_linux::SockType::from(type_and_flags & 0x0f),
+                flags: litebox_common_linux::SockFlags::from_bits_truncate(type_and_flags & !0x0f),
+                protocol: if syscall_args[2] == 0 {
                     None
                 } else {
                     Some(litebox_common_linux::protocol_from_u8(
-                        syscall_args[3].truncate(),
+                        syscall_args[2].truncate(),
                     ))
                 },
             }
