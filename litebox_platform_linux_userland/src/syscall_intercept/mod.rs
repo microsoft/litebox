db3861cae37b5d95145c3ad83f6432c02fceaa74
Jay Bosamiya <jayb@microsoft.com>
2025-04-25 18:47:05 -0700

    Get basic compilation working for 32-bit Linux userland

--- litebox_platform_linux_userland/src/syscall_intercept/mod.rs
+++ litebox_platform_linux_userland/src/syscall_intercept/mod.rs
@@ -1,6 +1,21 @@
 //! Implementation of syscall interception for Linux userland.
 
-mod systrap;
+#[cfg(target_arch = "x86_64")]
+pub(crate) mod systrap;
 
-pub(crate) use systrap::SYSCALL_ARG_MAGIC;
+#[cfg(target_arch = "x86")]
+pub(crate) mod systrap {
+    use litebox_common_linux::SyscallRequest;
+
+    pub(crate) const SYSCALL_ARG_MAGIC: u32 = u32::from_le_bytes(*b"LtBx");
+
+    pub(crate) fn init_sys_intercept(
+        _handler: impl Fn(SyscallRequest<crate::LinuxUserland>) -> i64 + Send + Sync + 'static,
+    ) {
+        // TODO: Actually start intercepting syscalls on 32-bit Linux.
+        //
+        // Temporarily, we are not setting anything up, while getting things compiling onto 32-bit Linux.
+    }
+}
+
 pub(crate) use systrap::init_sys_intercept;
