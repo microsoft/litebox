7d18f6f8785a17f889a188acc6d2379ab3927d41
Jay Bosamiya <jayb@microsoft.com>
2025-06-24 19:35:56 -0700

    Use the updated FDs in the shim

--- litebox_shim_linux/src/lib.rs
+++ litebox_shim_linux/src/lib.rs
@@ -45,6 +45,8 @@ type LinuxFS = litebox::fs::layered::FileSystem<
     >,
 >;
 
+type FileFd = litebox::fd::TypedFd<LinuxFS>;
+
 /// Get the global litebox object
 pub fn litebox<'a>() -> &'a LiteBox<Platform> {
     static LITEBOX: OnceBox<LiteBox<Platform>> = OnceBox::new();
@@ -73,7 +75,7 @@ pub fn set_fs(fs: LinuxFS) {
 /// # Panics
 ///
 /// Panics if this is called before [`set_fs`] has been called
-pub fn litebox_fs<'a>() -> &'a impl litebox::fs::FileSystem {
+pub fn litebox_fs<'a>() -> &'a LinuxFS {
     FS.get().expect("fs has not yet been set")
 }
 
@@ -169,7 +171,7 @@ impl Descriptors {
         let fd = fd as usize;
         self.descriptors.get_mut(fd)?.take()
     }
-    fn remove_file(&mut self, fd: u32) -> Option<litebox::fd::FileFd> {
+    fn remove_file(&mut self, fd: u32) -> Option<FileFd> {
         let fd = fd as usize;
         if let Some(Descriptor::File(file_fd)) = self
             .descriptors
@@ -196,7 +198,7 @@ impl Descriptors {
     fn get_fd(&self, fd: u32) -> Option<&Descriptor> {
         self.descriptors.get(fd as usize)?.as_ref()
     }
-    fn get_file_fd(&self, fd: u32) -> Option<&litebox::fd::FileFd> {
+    fn get_file_fd(&self, fd: u32) -> Option<&FileFd> {
         if let Descriptor::File(file_fd) = self.descriptors.get(fd as usize)?.as_ref()? {
             Some(file_fd)
         } else {
@@ -213,7 +215,7 @@ impl Descriptors {
 }
 
 enum Descriptor {
-    File(litebox::fd::FileFd),
+    File(FileFd),
     // Note we are using `Arc` here so that we can hold a reference to the socket
     // without holding a lock on the file descriptor (see `sys_accept` for an example).
     // TODO: this could be addressed by #120.
