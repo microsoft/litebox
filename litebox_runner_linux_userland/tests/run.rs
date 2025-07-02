c6975ccfebb3f65b394df9a0569a89bf486cf654
weitengchen <weitengchen@microsoft.com>
2025-07-02 11:04:46 -0700

    add rewriter backend for runner

--- litebox_runner_linux_userland/tests/run.rs
+++ litebox_runner_linux_userland/tests/run.rs
@@ -29,13 +29,40 @@ fn compile(input: &str, output: &str, exec_or_lib: bool) {
     );
 }
 
-#[test]
-fn test_runner_with_dynamic_lib() {
+#[expect(dead_code)]
+enum Backend {
+    Rewriter,
+    Seccomp,
+}
+
+#[allow(clippy::too_many_lines)]
+fn test_runner_with_dynamic_lib(backend: Backend) {
     let dir_path = std::env::var("OUT_DIR").unwrap();
     let src_path = std::path::Path::new(dir_path.as_str()).join("hello_exec.c");
     std::fs::write(src_path.clone(), HELLO_WORLD_C).unwrap();
     let path = std::path::Path::new(dir_path.as_str()).join("hello_dylib");
     compile(src_path.to_str().unwrap(), path.to_str().unwrap(), false);
+    let path = match backend {
+        Backend::Seccomp => path,
+        Backend::Rewriter => {
+            let output = std::process::Command::new("cargo")
+                .args([
+                    "run",
+                    "-p",
+                    "litebox_syscall_rewriter",
+                    "--",
+                    path.to_str().unwrap(),
+                ])
+                .output()
+                .expect("Failed to run litebox_syscall_rewriter");
+            assert!(
+                output.status.success(),
+                "failed to run litebox_syscall_rewriter {:?}",
+                std::str::from_utf8(output.stderr.as_slice()).unwrap()
+            );
+            std::path::Path::new(dir_path.as_str()).join("hello_dylib.hooked")
+        }
+    };
 
     // create tar file containing all dependencies
     let tar_dir = std::path::Path::new(dir_path.as_str()).join("tar_files");
@@ -53,13 +80,42 @@ fn test_runner_with_dynamic_lib() {
     for file in files_to_install {
         let file_path = std::path::Path::new(file);
         let dest_path = tar_dir.join(&file[1..]);
-        println!(
-            "Copying {} to {}",
-            file_path.to_str().unwrap(),
-            dest_path.to_str().unwrap()
-        );
-        std::fs::copy(file_path, dest_path).unwrap();
+        match backend {
+            Backend::Seccomp => {
+                println!(
+                    "Copying {} to {}",
+                    file_path.to_str().unwrap(),
+                    dest_path.to_str().unwrap()
+                );
+                std::fs::copy(file_path, dest_path).unwrap();
+            }
+            Backend::Rewriter => {
+                println!(
+                    "Running `cargo run -p litebox_syscall_rewriter -- -o {} {}`",
+                    dest_path.to_str().unwrap(),
+                    file_path.to_str().unwrap(),
+                );
+                let output = std::process::Command::new("cargo")
+                    .args([
+                        "run",
+                        "-p",
+                        "litebox_syscall_rewriter",
+                        "--",
+                        "-o",
+                        dest_path.to_str().unwrap(),
+                        file_path.to_str().unwrap(),
+                    ])
+                    .output()
+                    .expect("Failed to run litebox_syscall_rewriter");
+                assert!(
+                    output.status.success(),
+                    "failed to run litebox_syscall_rewriter {:?}",
+                    std::str::from_utf8(output.stderr.as_slice()).unwrap()
+                );
+            }
+        }
     }
+
     // create tar file using `tar` command
     let tar_file = std::path::Path::new(dir_path.as_str()).join("rootfs.tar");
     let tar_data = std::process::Command::new("tar")
@@ -75,12 +131,17 @@ fn test_runner_with_dynamic_lib() {
     println!("Tar file created at: {}", tar_file.to_str().unwrap());
 
     #[cfg(target_arch = "x86_64")]
-    let target = ""; // no need to specify target for x86_64
+    let target = "--target=x86_64-unknown-linux-musl";
     #[cfg(target_arch = "x86")]
-    let target = "--target=i686-unknown-linux-gnu";
+    let target = "--target=i686-unknown-linux-musl";
+    let backend = match backend {
+        Backend::Rewriter => "rewriter",
+        Backend::Seccomp => "seccomp",
+    };
     println!(
-        "Running `cargo run -p litebox_runner_linux_userland {} -- --unstable --initial-files {} {}`",
+        "Running `cargo run -p litebox_runner_linux_userland {} -- --unstable --interception-backend {} --initial-files {} --env LD_LIBRARY_PATH=/lib64:/lib32:/lib {}`",
         target,
+        backend,
         tar_file.to_str().unwrap(),
         path.to_str().unwrap()
     );
@@ -90,10 +151,16 @@ fn test_runner_with_dynamic_lib() {
             "run",
             "-p",
             "litebox_runner_linux_userland",
-            #[cfg(target_arch = "x86")]
-            "--target=i686-unknown-linux-gnu",
+            target,
             "--",
             "--unstable",
+            "--interception-backend",
+            backend,
+            // Tell ld where to find the libraries.
+            // See https://man7.org/linux/man-pages/man8/ld.so.8.html for how ld works.
+            // Alternatively, we could add a `/etc/ld.so.cache` file to the rootfs.
+            "--env",
+            "LD_LIBRARY_PATH=/lib64:/lib32:/lib",
             "--initial-files",
             tar_file.to_str().unwrap(),
             path.to_str().unwrap(),
@@ -105,4 +172,15 @@ fn test_runner_with_dynamic_lib() {
         "failed to run litebox_runner_linux_userland {:?}",
         std::str::from_utf8(output.stderr.as_slice()).unwrap()
     );
+}
+
+// TODO: enable this test once the rewriter is working.
+// #[test]
+// fn test_runner_with_dynamic_lib_rewriter() {
+//     test_runner_with_dynamic_lib(Backend::Rewriter);
+// }
+
+#[test]
+fn test_runner_with_dynamic_lib_seccomp() {
+    test_runner_with_dynamic_lib(Backend::Seccomp);
 }
