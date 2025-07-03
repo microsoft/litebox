6a7c147a62f7bfe0c9737cef37b1a892e837ddf4
icegrave0391 <zhangchuqi1999@gmail.com>
2025-07-03 12:28:12 -0700

    remove debug

--- litebox_shim_linux/src/syscalls/mm.rs
+++ litebox_shim_linux/src/syscalls/mm.rs
@@ -1,7 +1,4 @@
 //! Implementation of memory management related syscalls, eg., `mmap`, `munmap`, etc.
-
-use core::panic;
-
 use litebox::{
     mm::linux::{MappingError, PAGE_SIZE, VmemUnmapError},
     platform::{RawConstPointer, RawMutPointer, page_mgmt::DeallocationError},
@@ -114,7 +111,6 @@ pub(crate) fn sys_mmap(
 ) -> Result<MutPtr<u8>, Errno> {
     // check alignment
     if offset & !PAGE_MASK != 0 {
-        panic!("offset {:#x} is not aligned to page size", offset);
         return Err(Errno::EINVAL);
     }
     if addr & !PAGE_MASK != 0 {
