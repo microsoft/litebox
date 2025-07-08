f255cf107524cc1ff98cca7ef8a805df4ef4811c
Weiteng Chen <weitengchen@microsoft.com>
2025-07-08 11:54:21 -0700

    Update litebox/src/platform/mod.rs
    Co-authored-by: Jay Bosamiya (Microsoft) <jayb@microsoft.com>

--- litebox/src/platform/mod.rs
+++ litebox/src/platform/mod.rs
@@ -18,7 +18,8 @@ pub use page_mgmt::PageManagementProvider;
 #[macro_export]
 macro_rules! log_println {
     ($platform:expr, $($tt:tt)*) => {{
-        use core::fmt::Write;
+        use core::fmt::Write as _;
+        use $crate::platform::DebugLogProvider as _;
         let mut t: arrayvec::ArrayString<1024> = arrayvec::ArrayString::new();
         writeln!(t, $($tt)*).unwrap();
         $platform.debug_log_print(&t);
