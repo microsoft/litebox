2558eff0e722d01f811d0bae70be9bec832fc13f
icegrave0391 <zhangchuqi1999@gmail.com>
2025-07-15 17:36:32 -0700

    add runner helloworld testcase

---
+++ litebox_runner_linux_on_freebsd_userland/tests/common/mod.rs
@@ -0,0 +1,115 @@
+use std::{arch::global_asm, ffi::CString};
+
+use litebox::{
+    LiteBox,
+    fs::{FileSystem as _, Mode, OFlags},
+};
+use litebox_platform_multiplex::{Platform, set_platform};
+use litebox_shim_linux::{litebox_fs, loader::load_program, set_fs};
+
+#[cfg(target_arch = "x86_64")]
+global_asm!(
+    "
+    .text
+    .align	4
+    .globl	trampoline
+    .type	trampoline,@function
+trampoline:
+    xor rdx, rdx
+    mov	rsp, rsi
+    jmp	rdi
+    /* Should not reach. */
+    hlt"
+);
+#[cfg(target_arch = "x86")]
+global_asm!(
+    "
+    .text
+    .align  4
+    .globl  trampoline
+    .type   trampoline,@function
+trampoline:
+    xor     edx, edx
+    mov     ebx, [esp + 4]
+    mov     eax, [esp + 8]
+    mov     esp, eax
+    jmp     ebx
+    /* Should not reach. */
+    hlt"
+);
+
+unsafe extern "C" {
+    fn trampoline(entry: usize, sp: usize) -> !;
+}
+
+pub fn init_platform(tar_data: &[u8], initial_dirs: &[&str], initial_files: &[&str]) {
+    let platform = Platform::new();
+    set_platform(platform);
+    let platform = litebox_platform_multiplex::platform();
+    let litebox = LiteBox::new(platform);
+
+    let mut in_mem_fs = litebox::fs::in_mem::FileSystem::new(&litebox);
+    in_mem_fs.with_root_privileges(|fs| {
+        fs.chmod("/", Mode::RWXU | Mode::RWXG | Mode::RWXO)
+            .expect("Failed to set permissions on root");
+    });
+    let dev_stdio = litebox::fs::devices::stdio::FileSystem::new(&litebox);
+    let tar_ro_fs = litebox::fs::tar_ro::FileSystem::new(
+        &litebox,
+        if tar_data.is_empty() {
+            litebox::fs::tar_ro::empty_tar_file()
+        } else {
+            tar_data.into()
+        },
+    );
+    set_fs(litebox::fs::layered::FileSystem::new(
+        &litebox,
+        in_mem_fs,
+        litebox::fs::layered::FileSystem::new(
+            &litebox,
+            dev_stdio,
+            tar_ro_fs,
+            litebox::fs::layered::LayeringSemantics::LowerLayerReadOnly,
+        ),
+        litebox::fs::layered::LayeringSemantics::LowerLayerWritableFiles,
+    ));
+
+    for each in initial_dirs {
+        install_dir(each);
+    }
+    for each in initial_files {
+        let data = std::fs::read(each).unwrap();
+        install_file(data, each);
+    }
+
+    platform.register_syscall_handler(litebox_shim_linux::handle_syscall_request);
+}
+
+pub fn install_dir(path: &str) {
+    litebox_fs()
+        .mkdir(path, Mode::RWXU | Mode::RWXG | Mode::RWXO)
+        .expect("Failed to create directory");
+}
+
+pub fn install_file(contents: Vec<u8>, out: &str) {
+    let fd = litebox_fs()
+        .open(
+            out,
+            OFlags::CREAT | OFlags::WRONLY,
+            Mode::RWXG | Mode::RWXO | Mode::RWXU,
+        )
+        .unwrap();
+    litebox_fs().write(&fd, &contents, None).unwrap();
+    litebox_fs().close(fd).unwrap();
+}
+
+pub fn test_load_exec_common(executable_path: &str) {
+    let argv = vec![
+        CString::new(executable_path).unwrap(),
+        CString::new("hello").unwrap(),
+    ];
+    let envp = vec![CString::new("PATH=/bin").unwrap()];
+    let info = load_program(executable_path, argv, envp).unwrap();
+
+    unsafe { trampoline(info.entry_point, info.user_stack_top) };
+}
---
+++ litebox_runner_linux_on_freebsd_userland/tests/loader.rs
@@ -0,0 +1,129 @@
+mod common;
+
+#[expect(
+    unused,
+    reason = "This code snippet is just used to illustrate the source code of the `hello_exec_nolibc` test."
+)]
+const HELLO_WORLD_NOLIBC: &str = r#"
+// gcc tests/test.c -o test -static -nostdlib (-m32)
+#if defined(__x86_64__)
+int write(int fd, const char *buf, int length)
+{
+    int ret;
+
+    asm("mov %1, %%eax\n\t"
+        "mov %2, %%edi\n\t"
+        "mov %3, %%rsi\n\t"
+        "mov %4, %%edx\n\t"
+        "syscall\n\t"
+        "mov %%eax, %0"
+        : "=r" (ret)
+        : "i" (1), // #define SYS_write 1
+          "r" (fd),
+          "r" (buf),
+          "r" (length)
+        : "%eax", "%edi", "%rsi", "%edx");
+
+    return ret;
+}
+
+_Noreturn void exit_group(int code)
+{
+    /* Infinite for-loop since this function can't return */
+    for (;;) {
+        asm("mov %0, %%eax\n\t"
+            "mov %1, %%edi\n\t"
+            "syscall\n\t"
+            :
+            : "i" (231), // #define SYS_exit_group 231
+              "r" (code)
+            : "%eax", "%edi");
+    }
+}
+#elif defined(__i386__)
+int write(int fd, const char *buf, int length)
+{
+    int ret;
+
+    asm("mov %1, %%eax\n\t"
+        "mov %2, %%ebx\n\t"
+        "mov %3, %%ecx\n\t"
+        "mov %4, %%edx\n\t"
+        "int $0x80\n\t"
+        "mov %%eax, %0"
+        : "=r" (ret)
+        : "i" (4), // #define SYS_write 4
+          "g" (fd),
+          "g" (buf),
+          "g" (length)
+        : "%eax", "%ebx", "%ecx", "%edx");
+
+    return ret;
+}
+_Noreturn void exit_group(int code)
+{
+    /* Infinite for-loop since this function can't return */
+    for (;;) {
+        asm("mov %0, %%eax\n\t"
+            "mov %1, %%ebx\n\t"
+            "int $0x80\n\t"
+            :
+            : "i" (252), // #define SYS_exit_group 252
+              "r" (code)
+            : "%eax", "%ebx");
+    }
+}
+#else
+#error "Unsupported architecture"
+#endif
+
+int main() {
+    // use write to print a string
+    write(1, "Hello, World!\n", 14);
+    return 0;
+}
+
+void _start() {
+    exit_group(main());
+}
+"#;
+
+#[test]
+fn test_syscall_rewriter() {
+    // Use the already compiled executable from the tests folder (same dir as this file)
+    let mut test_dir = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    test_dir.push("tests");
+    let path = test_dir.join("hello_exec_nolibc");
+    let hooked_path = test_dir.join("hello_exec_nolibc.hooked");
+
+    // rewrite the hello_exec_nolibc
+    let _ = std::fs::remove_file(hooked_path.clone());
+    let output = std::process::Command::new("cargo")
+        .args([
+            "run",
+            "-p",
+            "litebox_syscall_rewriter",
+            "--",
+            "--trampoline-addr",
+            litebox_shim_linux::loader::REWRITER_MAGIC_NUMBER
+                .to_string()
+                .as_str(),
+            "-o",
+            hooked_path.to_str().unwrap(),
+            path.to_str().unwrap(),
+        ])
+        .output()
+        .expect("Failed to run syscall rewriter");
+    assert!(
+        output.status.success(),
+        "failed to run syscall rewriter {:?}",
+        std::str::from_utf8(output.stderr.as_slice()).unwrap()
+    );
+
+    let executable_path = "/hello_exec_nolibc.hooked";
+    let executable_data = std::fs::read(hooked_path).unwrap();
+
+    common::init_platform(&[], &[], &[]);
+    common::install_file(executable_data, executable_path);
+    common::test_load_exec_common(executable_path);
+}
