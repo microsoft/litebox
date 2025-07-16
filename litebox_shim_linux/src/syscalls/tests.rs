cef84cd86bc6be446912b8272dd14cd07496e6bd
icegrave0391 <zhangchuqi1999@gmail.com>
2025-07-15 21:35:10 -0700

    cargo fmt

--- litebox_shim_linux/src/syscalls/tests.rs
+++ litebox_shim_linux/src/syscalls/tests.rs
@@ -13,7 +13,7 @@ pub(crate) fn init_platform(tun_device_name: Option<&str>) {
     INIT_FUNC.call_once(|| {
         #[cfg(target_os = "linux")]
         set_platform(Platform::new(tun_device_name));
-        
+
         #[cfg(not(target_os = "linux"))]
         set_platform(Platform::new());
 
