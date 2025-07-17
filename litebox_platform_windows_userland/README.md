3d44473fd24d4aa06c589c483a7e1f72e42bec85
icegrave0391 <zhangchuqi1999@gmail.com>
2025-07-17 16:28:44 -0700

    template windows

---
+++ litebox_platform_windows_userland/README.md
@@ -0,0 +1,3 @@
+# A LiteBox Platform for running LiteBox on userland Windows
+
+This crate provides an instantiation of the LiteBox `platform::Provider`.
---
+++ litebox_platform_windows_userland/src/lib.rs
@@ -0,0 +1,554 @@
+//! A [LiteBox platform](../litebox/platform/index.html) for running LiteBox on userland Windows.
+
+// Restrict this crate to only work on Windows. For now, we are restricting this to only x86-64
+// Windows, but we _may_ allow for more in the future, if we find it useful to do so.
+#![cfg(all(target_os = "windows", target_arch = "x86_64"))]
+
+use core::sync::atomic::AtomicU32;
+use core::time::Duration;
+
+use litebox::platform::ImmediatelyWokenUp;
+use litebox::platform::page_mgmt::MemoryRegionPermissions;
+use litebox::platform::trivial_providers::TransparentMutPtr;
+use litebox::platform::{ThreadLocalStorageProvider, UnblockedOrTimedOut};
+use litebox_common_linux::{ProtFlags, PunchthroughSyscall};
+
+extern crate alloc;
+
+/// Connector to a shim-exposed syscall-handling interface.
+pub type SyscallHandler = fn(litebox_common_linux::SyscallRequest<WindowsUserland>) -> isize;
+
+/// The syscall handler passed down from the shim.
+static SYSCALL_HANDLER: std::sync::RwLock<Option<SyscallHandler>> = std::sync::RwLock::new(None);
+
+/// The userland Windows platform.
+///
+/// This implements the main [`litebox::platform::Provider`] trait, i.e., implements all platform
+/// traits.
+pub struct WindowsUserland {
+    /// Reserved pages that are not available for guest programs to use.
+    reserved_pages: Vec<core::ops::Range<usize>>,
+}
+
+const WINDOWS_PROC_MAPS_PATH: &str = ""; // Windows doesn't have /proc
+
+impl WindowsUserland {
+    /// Create a new userland-Windows platform for use in `LiteBox`.
+    pub fn new() -> &'static Self {
+        let platform = Self {
+            reserved_pages: Self::read_memory_maps(),
+        };
+
+        platform.set_init_tls();
+        Box::leak(Box::new(platform))
+    }
+
+    /// Register the syscall handler (provided by the Linux shim)
+    ///
+    /// # Panics
+    ///
+    /// Panics if the function has already been invoked earlier.
+    pub fn register_syscall_handler(&self, syscall_handler: SyscallHandler) {
+        let old = SYSCALL_HANDLER.write().unwrap().replace(syscall_handler);
+        assert!(
+            old.is_none(),
+            "Should not register more than one syscall_handler"
+        );
+    }
+
+    fn read_memory_maps() -> alloc::vec::Vec<core::ops::Range<usize>> {
+        // TODO: Implement Windows memory mapping discovery
+        // Windows doesn't have /proc, need to use Windows APIs like VirtualQuery
+        // For now, return empty vector as placeholder
+        alloc::vec::Vec::new()
+    }
+
+    fn get_user_info() -> litebox_common_linux::Credentials {
+        // TODO: Implement Windows user credential discovery
+        // Windows doesn't have getuid/getgid, need to use Windows APIs
+        litebox_common_linux::Credentials {
+            uid: 0, // Placeholder
+            euid: 0, // Placeholder  
+            gid: 0, // Placeholder
+            egid: 0, // Placeholder
+        }
+    }
+
+    fn set_init_tls(&self) {
+        // TODO: Implement Windows thread ID discovery
+        // Windows doesn't have thr_self, need to use GetCurrentThreadId()
+        let tid: i32 = 0; // Placeholder
+
+        let task = alloc::boxed::Box::new(litebox_common_linux::Task {
+            tid,
+            clear_child_tid: None,
+            robust_list: None,
+            credentials: alloc::sync::Arc::new(Self::get_user_info()),
+        });
+
+        let tls = litebox_common_linux::ThreadLocalStorage::new(task);
+        self.set_thread_local_storage(tls);
+    }
+}
+
+impl litebox::platform::Provider for WindowsUserland {}
+
+impl litebox::platform::ExitProvider for WindowsUserland {
+    type ExitCode = i32;
+    const EXIT_SUCCESS: Self::ExitCode = 0;
+    const EXIT_FAILURE: Self::ExitCode = 1;
+
+    fn exit(&self, code: Self::ExitCode) -> ! {
+        let Self { reserved_pages: _ } = self;
+
+        // TODO: Implement Windows process exit
+        // For now, use standard process exit
+        std::process::exit(code);
+    }
+}
+
+impl litebox::platform::ThreadProvider for WindowsUserland {
+    type ExecutionContext = litebox_common_linux::PtRegs;
+    type ThreadArgs = litebox_common_linux::NewThreadArgs<WindowsUserland>;
+    type ThreadSpawnError = litebox_common_linux::errno::Errno;
+    type ThreadId = usize;
+
+    #[expect(unused_variables)]
+    #[expect(unused_mut)]
+    unsafe fn spawn_thread(
+        &self,
+        ctx: &litebox_common_linux::PtRegs,
+        stack: TransparentMutPtr<u8>,
+        stack_size: usize,
+        entry_point: usize,
+        mut thread_args: Box<Self::ThreadArgs>,
+    ) -> Result<usize, Self::ThreadSpawnError> {
+        unimplemented!("spawn_thread is not implemented for Windows yet.");
+    }
+
+    fn terminate_thread(&self, code: Self::ExitCode) -> ! {
+        unimplemented!(
+            "terminate_thread is not implemented for Windows yet. code: {}",
+            code
+        );
+    }
+}
+
+impl litebox::platform::RawMutexProvider for WindowsUserland {
+    type RawMutex = RawMutex;
+
+    fn new_raw_mutex(&self) -> Self::RawMutex {
+        RawMutex {
+            inner: AtomicU32::new(0),
+            num_to_wake_up: AtomicU32::new(0),
+        }
+    }
+}
+
+// A skeleton of a raw mutex for Windows.
+#[expect(dead_code)]
+pub struct RawMutex {
+    // The `inner` is the value shown to the outside world as an underlying atomic.
+    inner: AtomicU32,
+    num_to_wake_up: AtomicU32,
+}
+
+impl RawMutex {
+    fn block_or_maybe_timeout(
+        &self,
+        val: u32,
+        timeout: Option<Duration>,
+    ) -> Result<UnblockedOrTimedOut, ImmediatelyWokenUp> {
+        unimplemented!(
+            "block_or_maybe_timeout is not implemented for Windows yet. val: {}, timeout: {:?}",
+            val,
+            timeout
+        );
+    }
+}
+
+impl litebox::platform::RawMutex for RawMutex {
+    fn underlying_atomic(&self) -> &AtomicU32 {
+        &self.inner
+    }
+
+    fn wake_many(&self, n: usize) -> usize {
+        unimplemented!("wake_many is not implemented for Windows yet. n: {}", n);
+    }
+
+    fn block(&self, val: u32) -> Result<(), ImmediatelyWokenUp> {
+        match self.block_or_maybe_timeout(val, None) {
+            Ok(UnblockedOrTimedOut::Unblocked) => Ok(()),
+            Ok(UnblockedOrTimedOut::TimedOut) => unreachable!(),
+            Err(ImmediatelyWokenUp) => Err(ImmediatelyWokenUp),
+        }
+    }
+
+    fn block_or_timeout(
+        &self,
+        val: u32,
+        timeout: Duration,
+    ) -> Result<UnblockedOrTimedOut, ImmediatelyWokenUp> {
+        self.block_or_maybe_timeout(val, Some(timeout))
+    }
+}
+
+impl litebox::platform::IPInterfaceProvider for WindowsUserland {
+    fn send_ip_packet(&self, packet: &[u8]) -> Result<(), litebox::platform::SendError> {
+        unimplemented!(
+            "send_ip_packet is not implemented for Windows yet. packet length: {}",
+            packet.len()
+        );
+    }
+
+    fn receive_ip_packet(
+        &self,
+        packet: &mut [u8],
+    ) -> Result<usize, litebox::platform::ReceiveError> {
+        unimplemented!(
+            "receive_ip_packet is not implemented for Windows yet. packet length: {}",
+            packet.len()
+        );
+    }
+}
+
+impl litebox::platform::TimeProvider for WindowsUserland {
+    type Instant = Instant;
+
+    fn now(&self) -> Self::Instant {
+        Instant {
+            inner: std::time::Instant::now(),
+        }
+    }
+}
+
+pub struct Instant {
+    inner: std::time::Instant,
+}
+
+impl litebox::platform::Instant for Instant {
+    fn checked_duration_since(&self, earlier: &Self) -> Option<core::time::Duration> {
+        self.inner.checked_duration_since(earlier.inner)
+    }
+}
+
+#[expect(dead_code)]
+pub struct PunchthroughToken {
+    punchthrough: PunchthroughSyscall<WindowsUserland>,
+}
+
+impl litebox::platform::PunchthroughToken for PunchthroughToken {
+    type Punchthrough = PunchthroughSyscall<WindowsUserland>;
+    fn execute(
+        self,
+    ) -> Result<
+        <Self::Punchthrough as litebox::platform::Punchthrough>::ReturnSuccess,
+        litebox::platform::PunchthroughError<
+            <Self::Punchthrough as litebox::platform::Punchthrough>::ReturnFailure,
+        >,
+    > {
+        unimplemented!("punchthrough is not implemented for Windows yet");
+    }
+}
+
+impl litebox::platform::PunchthroughProvider for WindowsUserland {
+    type PunchthroughToken = PunchthroughToken;
+    fn get_punchthrough_token_for(
+        &self,
+        punchthrough: <Self::PunchthroughToken as litebox::platform::PunchthroughToken>::Punchthrough,
+    ) -> Option<Self::PunchthroughToken> {
+        Some(PunchthroughToken { punchthrough })
+    }
+}
+
+impl litebox::platform::DebugLogProvider for WindowsUserland {
+    fn debug_log_print(&self, msg: &str) {
+        // TODO: Implement Windows debug logging
+        // For now, use standard error output
+        use std::io::Write;
+        let _ = std::io::stderr().write_all(msg.as_bytes());
+    }
+}
+
+impl litebox::platform::RawPointerProvider for WindowsUserland {
+    type RawConstPointer<T: Clone> = litebox::platform::trivial_providers::TransparentConstPtr<T>;
+    type RawMutPointer<T: Clone> = litebox::platform::trivial_providers::TransparentMutPtr<T>;
+}
+
+fn prot_flags(flags: MemoryRegionPermissions) -> ProtFlags {
+    let mut res = ProtFlags::PROT_NONE;
+    res.set(
+        ProtFlags::PROT_READ,
+        flags.contains(MemoryRegionPermissions::READ),
+    );
+    res.set(
+        ProtFlags::PROT_WRITE,
+        flags.contains(MemoryRegionPermissions::WRITE),
+    );
+    res.set(
+        ProtFlags::PROT_EXEC,
+        flags.contains(MemoryRegionPermissions::EXEC),
+    );
+    if flags.contains(MemoryRegionPermissions::SHARED) {
+        unimplemented!()
+    }
+    res
+}
+
+impl<const ALIGN: usize> litebox::platform::PageManagementProvider<ALIGN> for WindowsUserland {
+    fn allocate_pages(
+        &self,
+        range: core::ops::Range<usize>,
+        initial_permissions: MemoryRegionPermissions,
+        can_grow_down: bool,
+        populate_pages: bool,
+    ) -> Result<Self::RawMutPointer<u8>, litebox::platform::page_mgmt::AllocationError> {
+        unimplemented!(
+            "allocate_pages is not implemented for Windows yet. range: {:?}, permissions: {:?}, can_grow_down: {}, populate_pages: {}",
+            range,
+            initial_permissions,
+            can_grow_down,
+            populate_pages
+        );
+    }
+
+    unsafe fn deallocate_pages(
+        &self,
+        range: core::ops::Range<usize>,
+    ) -> Result<(), litebox::platform::page_mgmt::DeallocationError> {
+        unimplemented!(
+            "deallocate_pages is not implemented for Windows yet. range: {:?}",
+            range
+        );
+    }
+
+    unsafe fn remap_pages(
+        &self,
+        old_range: core::ops::Range<usize>,
+        new_range: core::ops::Range<usize>,
+    ) -> Result<Self::RawMutPointer<u8>, litebox::platform::page_mgmt::RemapError> {
+        unimplemented!(
+            "remap_pages is not implemented for Windows yet. old_range: {:?}, new_range: {:?}",
+            old_range,
+            new_range
+        );
+    }
+
+    unsafe fn update_permissions(
+        &self,
+        range: core::ops::Range<usize>,
+        new_permissions: MemoryRegionPermissions,
+    ) -> Result<(), litebox::platform::page_mgmt::PermissionUpdateError> {
+        unimplemented!(
+            "update_permissions is not implemented for Windows yet. range: {:?}, permissions: {:?}",
+            range,
+            new_permissions
+        );
+    }
+
+    fn reserved_pages(&self) -> impl Iterator<Item = &core::ops::Range<usize>> {
+        self.reserved_pages.iter()
+    }
+}
+
+impl litebox::platform::StdioProvider for WindowsUserland {
+    fn read_from_stdin(&self, buf: &mut [u8]) -> Result<usize, litebox::platform::StdioReadError> {
+        use std::io::Read as _;
+        std::io::stdin().read(buf).map_err(|err| {
+            if err.kind() == std::io::ErrorKind::BrokenPipe {
+                litebox::platform::StdioReadError::Closed
+            } else {
+                panic!("unhandled error {err}")
+            }
+        })
+    }
+
+    fn write_to(
+        &self,
+        stream: litebox::platform::StdioOutStream,
+        buf: &[u8],
+    ) -> Result<usize, litebox::platform::StdioWriteError> {
+        use std::io::Write as _;
+        match stream {
+            litebox::platform::StdioOutStream::Stdout => {
+                std::io::stdout().write(buf).map_err(|err| {
+                    if err.kind() == std::io::ErrorKind::BrokenPipe {
+                        litebox::platform::StdioWriteError::Closed
+                    } else {
+                        panic!("unhandled error {err}")
+                    }
+                })
+            }
+            litebox::platform::StdioOutStream::Stderr => {
+                std::io::stderr().write(buf).map_err(|err| {
+                    if err.kind() == std::io::ErrorKind::BrokenPipe {
+                        litebox::platform::StdioWriteError::Closed
+                    } else {
+                        panic!("unhandled error {err}")
+                    }
+                })
+            }
+        }
+    }
+
+    fn is_a_tty(&self, stream: litebox::platform::StdioStream) -> bool {
+        use litebox::platform::StdioStream;
+        use std::io::IsTerminal as _;
+        match stream {
+            StdioStream::Stdin => std::io::stdin().is_terminal(),
+            StdioStream::Stdout => std::io::stdout().is_terminal(),
+            StdioStream::Stderr => std::io::stderr().is_terminal(),
+        }
+    }
+}
+
+#[global_allocator]
+static SLAB_ALLOC: litebox::mm::allocator::SafeZoneAllocator<'static, 28, WindowsUserland> =
+    litebox::mm::allocator::SafeZoneAllocator::new();
+
+impl litebox::mm::allocator::MemoryProvider for WindowsUserland {
+    fn alloc(layout: &std::alloc::Layout) -> Option<(usize, usize)> {
+        unimplemented!(
+            "Memory allocation is not implemented for Windows yet. layout: {:?}",
+            layout
+        );
+    }
+
+    unsafe fn free(_addr: usize) {
+        unimplemented!("Memory deallocation is not implemented for Windows yet.");
+    }
+}
+
+// TODO: Windows syscall assembly callback - needs Windows-specific implementation
+// Windows uses different calling conventions and syscall mechanisms
+/*
+core::arch::global_asm!(
+    "
+    // Windows x64 calling convention syscall callback would go here
+    // This needs to be implemented with Windows-specific assembly
+    "
+);
+*/
+
+unsafe extern "C" {
+    // TODO: Define Windows syscall callback when assembly is implemented
+    // fn syscall_callback() -> isize;
+}
+
+/// Windows syscall handler (placeholder - needs Windows implementation)
+///
+/// # Safety
+///
+/// - The `ctx` pointer must be valid pointer to a `litebox_common_linux::PtRegs` structure.
+/// - If any syscall argument is a pointer, it must be valid.
+///
+/// # Panics
+///
+/// Unsupported syscalls or arguments would trigger a panic for development purposes.
+#[unsafe(no_mangle)]
+unsafe extern "C" fn syscall_handler(
+    syscall_number: usize,
+    ctx: *mut litebox_common_linux::PtRegs,
+) -> isize {
+    unimplemented!(
+        "Windows syscall handler not implemented yet. syscall_number: {}, ctx: {:?}",
+        syscall_number,
+        ctx
+    );
+}
+
+impl litebox::platform::SystemInfoProvider for WindowsUserland {
+    fn get_syscall_entry_point(&self) -> usize {
+        unimplemented!("Windows syscall entry point not implemented yet");
+    }
+
+    fn get_vdso_address(&self) -> Option<usize> {
+        // Windows doesn't have VDSO equivalent, return None
+        None
+    }
+}
+
+impl WindowsUserland {
+    fn get_thread_local_storage() -> *mut litebox_common_linux::ThreadLocalStorage<WindowsUserland>
+    {
+        unimplemented!("Windows TLS access not implemented yet");
+    }
+}
+
+/// Windows thread-local storage implementation (placeholder)
+/// Windows uses different TLS mechanisms than Unix systems
+#[expect(unused)]
+impl litebox::platform::ThreadLocalStorageProvider for WindowsUserland {
+    type ThreadLocalStorage = litebox_common_linux::ThreadLocalStorage<WindowsUserland>;
+
+    fn set_thread_local_storage(&self, tls: Self::ThreadLocalStorage) {
+        unimplemented!("Windows TLS setting not implemented yet");
+    }
+
+    fn release_thread_local_storage(&self) -> Self::ThreadLocalStorage {
+        unimplemented!("Windows TLS release not implemented yet");
+    }
+
+    fn with_thread_local_storage_mut<F, R>(&self, f: F) -> R
+    where
+        F: FnOnce(&mut Self::ThreadLocalStorage) -> R,
+    {
+        unimplemented!("Windows TLS access not implemented yet");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use core::sync::atomic::AtomicU32;
+    use litebox::platform::RawMutex;
+    use litebox::platform::ThreadLocalStorageProvider as _;
+    use std::thread::sleep;
+
+    use crate::WindowsUserland;
+    use litebox::platform::{DebugLogProvider, PageManagementProvider};
+
+    extern crate std;
+
+    #[test]
+    #[should_panic(expected = "not implemented")]
+    fn test_raw_mutex() {
+        let mutex = std::sync::Arc::new(super::RawMutex {
+            inner: AtomicU32::new(0),
+            num_to_wake_up: AtomicU32::new(0),
+        });
+
+        let copied_mutex = mutex.clone();
+        std::thread::spawn(move || {
+            sleep(core::time::Duration::from_millis(500));
+            copied_mutex.wake_many(10);
+        });
+
+        assert!(mutex.block(0).is_ok());
+    }
+
+    #[test]
+    fn test_reserved_pages() {
+        let platform = WindowsUserland::new();
+
+        platform.debug_log_print("msg from WindowsUserland test_reserved_pages\n");
+
+        let reserved_pages: Vec<_> =
+            <WindowsUserland as PageManagementProvider<4096>>::reserved_pages(platform).collect();
+
+        // Check that the reserved pages are in order and non-overlapping
+        let mut prev = 0;
+        for page in reserved_pages {
+            assert!(page.start >= prev);
+            assert!(page.end > page.start);
+            prev = page.end;
+        }
+    }
+
+    #[test]
+    #[should_panic(expected = "not implemented")]
+    fn test_tls() {
+        let platform = WindowsUserland::new();
+        let _tls = WindowsUserland::get_thread_local_storage();
+        // TLS operations will panic as they're not implemented
+    }
+}
