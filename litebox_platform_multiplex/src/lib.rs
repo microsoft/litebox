760d1fb7c2aaa939c20ab73a1f86b711137b65cf
icegrave0391 <zhangchuqi1999@gmail.com>
2025-07-22 14:16:06 -0700

    add Windows platform dependencies

--- litebox_platform_multiplex/src/lib.rs
+++ litebox_platform_multiplex/src/lib.rs
@@ -34,6 +34,9 @@ pub type Platform = litebox_platform_linux_userland::LinuxUserland;
 #[cfg(all(feature = "platform_freebsd_userland", target_os = "freebsd"))]
 pub type Platform = litebox_platform_freebsd_userland::FreeBSDUserland;
 
+#[cfg(all(feature = "platform_windows_userland", target_os = "windows"))]
+pub type Platform = litebox_platform_windows_userland::WindowsUserland;
+
 static PLATFORM: once_cell::race::OnceBox<&'static Platform> = once_cell::race::OnceBox::new();
 
 /// Initialize the shim by providing a [LiteBox platform](../litebox/platform/index.html).
