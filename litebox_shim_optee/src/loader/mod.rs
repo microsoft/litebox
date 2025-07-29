b07813d889e8b928517edf9dfd8a77cd073887d0
Sangho Lee <Sangho.Lee@microsoft.com>
2025-07-29 16:22:38 +0000

    avoid redundant memory copies

--- litebox_shim_optee/src/loader/mod.rs
+++ litebox_shim_optee/src/loader/mod.rs
@@ -3,20 +3,8 @@
 mod elf;
 mod stack;
 
-// Load a program into the LiteBox shim.
-// pub fn load_program(
-//     path: &str,
-//     argv: alloc::vec::Vec<alloc::ffi::CString>,
-//     envp: alloc::vec::Vec<alloc::ffi::CString>,
-// ) -> Result<elf::ElfLoadInfo, elf::ElfLoaderError> {
-//     elf::ElfLoader::load(path, argv, envp, aux)
-// }
-
-pub fn load_elf_buffer(
-    elf_buf: &[u8],
-    argv: alloc::vec::Vec<alloc::ffi::CString>,
-) -> Result<elf::ElfLoadInfo, elf::ElfLoaderError> {
-    elf::ElfLoader::load_buffer(elf_buf, argv)
+pub fn load_elf_buffer(elf_buf: &[u8]) -> Result<elf::ElfLoadInfo, elf::ElfLoaderError> {
+    elf::ElfLoader::load_buffer(elf_buf)
 }
 
 pub(crate) const DEFAULT_STACK_SIZE: usize = 8 * 1024 * 1024; // 8 MB
