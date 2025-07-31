644a141d0e40e708c33109f348552b9e9a7146fd
Sangho Lee <Sangho.Lee@microsoft.com>
2025-07-31 16:19:20 +0000

    trigger ELF symbol relocation

--- litebox_shim_optee/src/syscalls/tee.rs
+++ litebox_shim_optee/src/syscalls/tee.rs
@@ -1,11 +1,23 @@
 use litebox_common_optee::TeeResult;
 
 // placeholder
-pub fn sys_return(_ret: usize) -> ! {
+pub fn sys_return(ret: usize) -> ! {
+    #[cfg(debug_assertions)]
+    litebox::log_println!(
+        litebox_platform_multiplex::platform(),
+        "sys_return: ret {}",
+        ret
+    );
     todo!("switch to VTL0");
 }
 
 pub fn sys_log(buf: &[u8]) -> Result<(), TeeResult> {
+    #[cfg(debug_assertions)]
+    litebox::log_println!(
+        litebox_platform_multiplex::platform(),
+        "sys_log: buf {:#x}",
+        buf.as_ptr() as usize
+    );
     let msg = core::str::from_utf8(buf).map_err(|_| TeeResult::BadFormat)?;
     litebox::log_println!(litebox_platform_multiplex::platform(), "{}", msg);
     Ok(())
