11322e2868ba442f4fdb10a4409c4de43fd9f7f9
Sangho Lee <Sangho.Lee@microsoft.com>
2025-08-01 14:20:32 +0000

    use a JSON file to pass TA command sequence

--- litebox_runner_optee_on_linux_userland/src/lib.rs
+++ litebox_runner_optee_on_linux_userland/src/lib.rs
@@ -3,14 +3,18 @@ use clap::Parser;
 use litebox_common_optee::{TeeParamType, UteeEntryFunc, UteeParams};
 use litebox_platform_multiplex::Platform;
 use litebox_shim_optee::{add_optee_command, add_session_id_elf_load_info};
+use serde::Deserialize;
 use std::path::PathBuf;
 
 /// Test OP-TEE TAs with LiteBox on unmodified Linux
 #[derive(Parser, Debug)]
 pub struct CliArgs {
-    /// The TA and arguments passed to it
-    #[arg(required = true, trailing_var_arg = true, value_hint = clap::ValueHint::CommandWithArguments)]
-    pub program_and_arguments: Vec<String>,
+    /// Trusted Application (TA)
+    #[arg(required = true, value_hint = clap::ValueHint::ExecutablePath)]
+    pub program: String,
+    /// JSON-formatted command sequence to pass to the TA
+    #[arg(required = true, value_hint = clap::ValueHint::FilePath)]
+    pub command_sequence: String,
     /// Allow using unstable options
     #[arg(short = 'Z', long = "unstable")]
     pub unstable: bool,
@@ -45,6 +49,24 @@ pub enum InterceptionBackend {
     Rewriter,
 }
 
+/// OP-TEE/TA message command. It consists of a function ID, command ID, and
+/// up to four arguments.
+#[derive(Debug, Deserialize)]
+struct TaCommand {
+    func_id: u32,
+    cmd_id: u32,
+    args: Vec<TaCommandArgument>,
+}
+
+/// An argument of OP-TEE/TA message command. It consists of a type and
+/// two 64-bit values/references.
+#[derive(Debug, Deserialize)]
+struct TaCommandArgument {
+    param_type: u8,
+    value_a: u64,
+    value_b: u64,
+}
+
 /// Test OP-TEE TAs with LiteBox on unmodified Linux
 ///
 /// # Panics
@@ -54,7 +76,7 @@ pub enum InterceptionBackend {
 /// message could be thrown instead.
 pub fn run(cli_args: CliArgs) -> Result<()> {
     let prog_data: Vec<u8> = {
-        let prog = PathBuf::from(&cli_args.program_and_arguments[0]);
+        let prog = PathBuf::from(&cli_args.program);
         let data = std::fs::read(prog).unwrap();
         #[allow(clippy::let_and_return)]
         let data = if cli_args.rewrite_syscalls {
@@ -67,6 +89,12 @@ pub fn run(cli_args: CliArgs) -> Result<()> {
             data
         };
         data
+    };
+
+    let ta_commands: Vec<TaCommand> = {
+        let json_path = PathBuf::from(&cli_args.command_sequence);
+        let json_str = std::fs::read_to_string(json_path)?;
+        serde_json::from_str(&json_str)?
     };
 
     // TODO(jb): Clean up platform initialization once we have https://github.com/MSRSSP/litebox/issues/24
@@ -82,30 +110,33 @@ pub fn run(cli_args: CliArgs) -> Result<()> {
         InterceptionBackend::Rewriter => {}
     }
 
-    // TODO: obtain these parameters, function ID, and command ID from the command line arguments or
-    // some other means (e.g., config file).
     let mut params = UteeParams::new();
     let loaded_program =
         litebox_shim_optee::loader::load_elf_buffer(prog_data.as_slice(), &params).unwrap();
 
-    add_session_id_elf_load_info(1, &loaded_program);
+    let session_id = 1; // use a fixed session ID for now
+    add_session_id_elf_load_info(session_id, &loaded_program);
 
-    params.set_type(0, TeeParamType::None).unwrap();
-    params.set_type(1, TeeParamType::None).unwrap();
-    params.set_type(2, TeeParamType::None).unwrap();
-    params.set_type(3, TeeParamType::None).unwrap();
-    add_optee_command(1, UteeEntryFunc::OpenSession, &params, 0);
+    for ta_command in ta_commands {
+        if ta_command.args.len() > 4 {
+            eprintln!("Warning: ta_command has more than four arguments!");
+        }
+        for (i, arg) in ta_command.args.iter().enumerate() {
+            let param_type = TeeParamType::try_from(arg.param_type).expect("Invalid param type");
+            params.set_type(i, param_type).unwrap();
+            params.set_values(i, arg.value_a, arg.value_b).unwrap();
+        }
+        for i in ta_command.args.len()..4 {
+            params.set_type(i, TeeParamType::None).unwrap();
+        }
 
-    params.set_type(0, TeeParamType::ValueInout).unwrap();
-    params.set_values(0, 100, 0).unwrap();
-    add_optee_command(1, UteeEntryFunc::InvokeCommand, &params, 0);
-
-    params.set_type(0, TeeParamType::ValueInout).unwrap();
-    params.set_values(0, 200, 0).unwrap();
-    add_optee_command(1, UteeEntryFunc::InvokeCommand, &params, 1);
-
-    params.set_type(0, TeeParamType::None).unwrap();
-    add_optee_command(1, UteeEntryFunc::CloseSession, &params, 0);
+        add_optee_command(
+            session_id,
+            UteeEntryFunc::try_from(ta_command.func_id).expect("Invalid function ID"),
+            &params,
+            ta_command.cmd_id,
+        );
+    }
 
     litebox_shim_optee::optee_command_loop();
 }
