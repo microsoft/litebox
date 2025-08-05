8893826b62e2a4fbff2114b5033e3d6b32a2db16
icegrave0391 <zhangchuqi1999@gmail.com>
2025-08-05 01:03:31 -0700

    fix build cfg

--- litebox_runner_linux_on_windows_userland/src/main.rs
+++ litebox_runner_linux_on_windows_userland/src/main.rs
@@ -1,3 +1,7 @@
+// Restrict this crate to only work on Windows. For now, we are restricting this to only x86-64
+// Windows, but we _may_ allow for more in the future, if we find it useful to do so.
+#![cfg(all(target_os = "windows", target_arch = "x86_64"))]
+
 use clap::Parser as _;
 use litebox_runner_linux_on_windows_userland::CliArgs;
 
