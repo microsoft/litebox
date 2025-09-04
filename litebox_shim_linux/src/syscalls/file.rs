1ce039e15baa8939ff5477e59275611a1d2316bb
icegrave0391 <zhangchuqi1999@gmail.com>
2025-09-03 17:17:38 -0700

    dup

--- litebox_shim_linux/src/syscalls/file.rs
+++ litebox_shim_linux/src/syscalls/file.rs
@@ -677,11 +677,8 @@ pub fn sys_fcntl(fd: i32, arg: FcntlArg) -> Result<u32, Errno> {
             Ok(0)
         }
         FcntlArg::DUPFD_CLOEXEC(minfd) => {
-            let new_file = file_descriptors()
-                .read()
-                .get_fd(fd)
-                .ok_or(Errno::EBADF)
-                .map(|desc| do_dup(desc, OFlags::CLOEXEC))?;
+            let new_file = do_dup(desc, OFlags::CLOEXEC);
+            
             drop(locked_file_descriptors);
             // dup and choose a fd since `minfd`
             Ok(file_descriptors().write().insert_since(new_file, minfd))
