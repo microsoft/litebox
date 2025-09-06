39362a1900db1552967eaa49779e3fd1665e126f
icegrave0391 <zhangchuqi1999@gmail.com>
2025-09-06 14:44:09 -0700

    correct errno

--- litebox_shim_linux/src/lib.rs
+++ litebox_shim_linux/src/lib.rs
@@ -339,7 +339,7 @@ pub fn handle_syscall_request(request: SyscallRequest<Platform>) -> usize {
             syscalls::file::sys_lseek(fd, offset, whence)
         }
         SyscallRequest::Mkdir { pathname, mode } => {
-            pathname.to_cstring().map_or(Err(Errno::EFAULT), |path| {
+            pathname.to_cstring().map_or(Err(Errno::EINVAL), |path| {
                 syscalls::file::sys_mkdir(path, mode).map(|()| 0)
             })
         }
