f785a26c3daa32fd57649bd3fb19554de6db04e0
Sangho Lee <Sangho.Lee@microsoft.com>
2025-10-31 17:23:11 +0000

    ratchet

--- dev_tests/src/ratchet.rs
+++ dev_tests/src/ratchet.rs
@@ -62,7 +62,7 @@ fn ratchet_maybe_uninit() -> Result<()> {
             ("dev_tests/", 1),
             ("litebox_platform_linux_kernel/", 1),
             ("litebox_platform_linux_userland/", 3),
-            ("litebox_platform_lvbs/", 5),
+            ("litebox_platform_lvbs/", 6),
             ("litebox_shim_linux/", 5),
         ],
         |file| {
