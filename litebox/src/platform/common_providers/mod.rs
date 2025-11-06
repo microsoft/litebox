f6346f91634b16f0157a2e4241f3e2258e31c521
weitengchen <weitengchen@microsoft.com>
2025-11-05 17:34:56 -0800

    add doc

--- litebox/src/platform/common_providers/mod.rs
+++ litebox/src/platform/common_providers/mod.rs
@@ -1,3 +1,7 @@
-//! Common platform provider
+//! Common Platform Provider Implementations
+//!
+//! This module contains reusable implementations of platform provider traits that
+//! can be shared across multiple platform implementations. These providers handle
+//! common functionality that doesn't require platform-specific customization.
 
 pub mod userspace_pointers;
