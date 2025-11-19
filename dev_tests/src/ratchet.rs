9097bb5b1fbd3c5830af64df88ceec8af87acca7
John Starks <jostarks@microsoft.com>
2025-11-19 19:00:56 +0000

    pay the ratchet man

--- dev_tests/src/ratchet.rs
+++ dev_tests/src/ratchet.rs
@@ -29,7 +29,7 @@ fn ratchet_globals() -> Result<()> {
             ("litebox/", 7),
             ("litebox_platform_linux_kernel/", 5),
             ("litebox_platform_linux_userland/", 6),
-            ("litebox_platform_lvbs/", 18),
+            ("litebox_platform_lvbs/", 19),
             ("litebox_platform_multiplex/", 1),
             ("litebox_platform_windows_userland/", 8),
             ("litebox_runner_linux_userland/", 1),
