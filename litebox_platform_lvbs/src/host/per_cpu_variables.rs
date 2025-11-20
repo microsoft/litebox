8d7c652d661131e620da610967410329af076b8f
Sangho Lee <sanghle@microsoft.com>
2025-11-20 04:51:48 +0000

    revert lvbs changes

--- litebox_platform_lvbs/src/host/per_cpu_variables.rs
+++ litebox_platform_lvbs/src/host/per_cpu_variables.rs
@@ -287,7 +287,6 @@ fn get_or_init_refcell_of_per_cpu_variables() -> Option<&'static RefCell<*mut Pe
         } else {
             let addr = x86_64::VirtAddr::new(&raw const *refcell as u64);
             unsafe { wrgsbase(usize::try_from(addr.as_u64()).unwrap()) }
-            crate::arch::write_kernel_gsbase_msr(addr);
             Some(refcell)
         }
     } else {
