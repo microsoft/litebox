095e591deeab8a7201a17526aa4e4db0443febfc
Angelina Vu <angelinavu@microsoft.com>
2025-11-21 18:34:19 +0000

    ratchet

--- dev_tests/src/ratchet.rs
+++ dev_tests/src/ratchet.rs
@@ -32,6 +32,7 @@ fn ratchet_globals() -> Result<()> {
             ("litebox_platform_multiplex/", 1),
             ("litebox_platform_windows_userland/", 7),
             ("litebox_runner_linux_userland/", 1),
+            ("litebox_runner_lvbs/", 3),
             ("litebox_shim_linux/", 10),
             ("litebox_shim_optee/", 6),
         ],
