5446026f2d3d15d6eaad68f3e2b51c4babf622e2
Sangho Lee <sanghle@microsoft.com>
2025-12-04 07:32:47 +0000

    revert

--- litebox_platform_lvbs/src/mshv/mod.rs
+++ litebox_platform_lvbs/src/mshv/mod.rs
@@ -11,7 +11,7 @@ mod heki;
 mod hvcall_mm;
 mod mem_integrity;
 
-use crate::host::{hv_hypercall_page_address, per_cpu_variables::KernelTlsOffset};
+use crate::host::hv_hypercall_page_address;
 use crate::mshv::hvcall_vp::hvcall_get_vp_registers;
 use crate::mshv::vtl1_mem_layout::PAGE_SIZE;
 use litebox_common_linux::errno::Errno;
@@ -850,6 +850,7 @@ impl HvPendingExceptionEvent {
     }
 }
 
+static mut VTL_RETURN_ADDRESS: u64 = 0;
 pub(crate) fn mshv_vsm_get_code_page_offsets() -> Result<(), Errno> {
     let value =
         hvcall_get_vp_registers(HV_REGISTER_VSM_CODEPAGE_OFFSETS).map_err(|_| Errno::EIO)?;
@@ -857,24 +858,20 @@ pub(crate) fn mshv_vsm_get_code_page_offsets() -> Result<(), Errno> {
     let vtl_return_address =
         hv_hypercall_page_address() + u64::from(code_page_offsets.vtl_return_offset());
     unsafe {
-        core::arch::asm!(
-            "mov gs:[{vtl_ret_addr_offset}], {vtl_return_address}",
-            vtl_ret_addr_offset = const {KernelTlsOffset::VtlReturnAddr as usize},
-            vtl_return_address = in(reg) vtl_return_address,
-            options(nostack, preserves_flags),
-        );
+        VTL_RETURN_ADDRESS = vtl_return_address;
     }
     Ok(())
 }
 
-#[unsafe(naked)]
-pub unsafe extern "C" fn vtl_return() {
-    core::arch::naked_asm!(
-        "mov rax, gs:[{vtl_ret_addr_offset}]",
-        "xor ecx, ecx",
-        "call rax",
-        vtl_ret_addr_offset = const { KernelTlsOffset::VtlReturnAddr as usize },
-    );
+#[expect(clippy::inline_always)]
+#[inline(always)]
+pub fn vtl_return() {
+    unsafe {
+        core::arch::asm!(
+            "call rax",
+            in("rax") VTL_RETURN_ADDRESS, in("rcx") 0x0,
+        );
+    }
 }
 
 #[cfg(test)]
