18d4a636267164e8fe76aacc09e88e30ded7c2e2
Sangho Lee <sanghle@microsoft.com>
2025-12-10 23:00:53 +0000

    msg_proto

--- litebox_shim_optee/src/lib.rs
+++ litebox_shim_optee/src/lib.rs
@@ -33,6 +33,8 @@ use litebox_platform_multiplex::Platform;
 pub mod loader;
 pub(crate) mod syscalls;
 
+pub mod msg_proto;
+
 const MAX_KERNEL_BUF_SIZE: usize = 0x80_000;
 
 pub struct OpteeShimEntrypoints {
