83cdabf7b73ecd4df6bc367c5bc711b5551e9387
weitengchen <weitengchen@microsoft.com>
2026-08-17 22:45:12 +0000

    fix rebase

--- litebox_shim_linux/src/syscalls/net.rs
+++ litebox_shim_linux/src/syscalls/net.rs
@@ -3359,7 +3359,7 @@ mod unix_tests {
     #[test]
     fn test_socketpair_race_with_concurrent_close() {
         let task = init_platform(None);
-        task.files.borrow().set_max_fd(3);
+        task.files.borrow().set_fd_limit(3);
 
         let stop = alloc::sync::Arc::new(core::sync::atomic::AtomicBool::new(false));
         let stop_closer = stop.clone();
