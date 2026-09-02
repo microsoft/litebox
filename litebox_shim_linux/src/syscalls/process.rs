12bec13f8a847292d5a2d2ffb1d45dd7181e619f
Weiteng Chen <weitengchen@microsoft.com>
2026-09-02 18:17:33 +0000

    Fix races in Linux file descriptor duplication (#1181) (#1259)
    Cherry-pick 377309f89a2daebabb882cc5a46bce286e3766bd from main.

--- litebox_shim_linux/src/syscalls/process.rs
+++ litebox_shim_linux/src/syscalls/process.rs
@@ -836,8 +836,7 @@ impl<Platform: ShimPlatform, FS: ShimFS> Task<Platform, FS> {
             }
             match resource {
                 litebox_common_linux::RlimitResource::NOFILE => {
-                    let new_max_fd = new_limit.rlim_cur.saturating_sub(1);
-                    self.files.borrow().set_max_fd(new_max_fd);
+                    self.files.borrow().set_max_fd(new_limit.rlim_cur);
                 }
                 _ => unimplemented!("Unsupported resource for set_rlimit: {:?}", resource),
             }
