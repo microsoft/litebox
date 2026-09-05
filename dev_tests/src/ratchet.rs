d38578aa6db28caea428354b5ecffc90756dfce8
Jay Bosamiya <jayb@microsoft.com>
2026-09-04 17:16:57 -0700

    Fix ratchet due to merge

--- dev_tests/src/ratchet.rs
+++ dev_tests/src/ratchet.rs
@@ -39,15 +39,15 @@ fn ratchet_globals() -> Result<()> {
             ("litebox/", 9),
             ("litebox_platform_linux_kernel/", 5),
             ("litebox_platform_linux_userland/", 5),
-            ("litebox_platform_lvbs/", 23),
+            ("litebox_platform_lvbs/", 22),
             ("litebox_platform_multiplex/", 1),
             ("litebox_platform_windows_userland/", 8),
             ("litebox_runner_lvbs/", 6),
             ("litebox_runner_snp/", 2),
             ("litebox_shim_linux/", 2),
-            ("litebox_shim_optee/", 4),
+            ("litebox_shim_optee/", 6),
             ("litebox_shim_windows/", 1),
-            ("litebox_runner_windows_userland/", 2)
+            ("litebox_runner_windows_userland/", 2),
         ],
         |file| {
             Ok(file
